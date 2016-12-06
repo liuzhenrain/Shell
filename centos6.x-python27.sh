1ead7a3c70e94c22a06ead1481126b8255e7adcd
Phillip Liu <liuzhenrain@gmail.com>
2016-12-06 15:39:04 +0800

    新增说明语句

--- centos6.x-python27.sh
+++ centos6.x-python27.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo 'this script will to install Python2.7.12 to centos 6.x'
 version='2.7.12'
+read -p "Enter your name:" name
+echo "Hello $name ,welcome to my program"
--- shellcollections.sh
+++ shellcollections.sh
@@ -9,3 +9,14 @@ chmod a-rwx $filepath
 sudo supervisorctl start shadowsocks #在aws上，因为我添加了supervisor，将shadowsocks变成一个服务了。
 # 修改shadowsocks.json配置
 sudo vi /etc/shadowsocks.json
+# centos 重启ssh服务
+/etc/init.d/sshd restart
+# 上传本地文件到服务端
+# linux/unix/Mac
+scp -P 22(如果是默认的不需要这个) /path/local_filename username@server_ip:/server_dir_path
+# windows 请使用git bush客户端
+sc -P 22(如果是默认22端口则不需要) /path/local_filename username@server_ip:/server_dir_path
+# 从服务器下载文件，将上传的命令反过来就行。
+
+# linux/unix/Mac 登录服务器
+ssh -p 22 username@server_ip -i /private_key_path
