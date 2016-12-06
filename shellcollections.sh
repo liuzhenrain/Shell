d9f771ee8cc6b1d62ebe1f1580ee79c4f7f531b4
Phillip Liu <liuzhenrain@gmail.com>
2016-12-06 10:41:38 +0800

    增加shadowsocks相关

--- shellcollections.sh
+++ shellcollections.sh
@@ -3,4 +3,9 @@ ls -al $filepath
 
 # 修改文件权限 我也不知道到底啥意思
 chmod 777 $filepath
-chmod a-rwx $filepath 
+chmod a-rwx $filepath
+
+# 重启，停止，开始Shadowscoks服务
+sudo supervisorctl start shadowsocks #在aws上，因为我添加了supervisor，将shadowsocks变成一个服务了。
+# 修改shadowsocks.json配置
+sudo vi /etc/shadowsocks.json
