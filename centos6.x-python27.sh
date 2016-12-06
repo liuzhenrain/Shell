c0081e67278b8a6ae6de27307bcc3567f3f13513
Philip Liu <liuzhenrain@gmail.com>
2016-12-07 00:10:02 +0800

    修改错误

--- centos6.x-python27.sh
+++ centos6.x-python27.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
+# 本文curl 地址 ：https://raw.githubusercontent.com/liuzhenrain/Shell/master/centos6.x-python27.sh
+# 不另外fork应该不会发生变化的
 echo 'this script will to install Python2.7.12 to centos 6.x'
 version='2.7.12'
-python_url = "https://www.python.org/ftp/python/$version/Python-${version}.tgz"
+python_url="https://www.python.org/ftp/python/$version/Python-${version}.tgz"
 
 # 更新yum
-echo "更新yum"
+echo "1.更新yum"
 yum -y update
 
-# 安装需要的工具或库文件
-echo "安装需要的工具或库文件"
-yum groupinstall -y "Development tools"
-yum install -y openssl-devel zlib-devel ncurses-devel bzip2-devel readline-devel
-yum install -y libtool-ltdl-devel sqlite-devel tk-devel tcl-devel xz-libs
-sleep 5
-
 # 先检查本地的Python版本
-echo "检查本地Python版本 $(python -V &2>1)"
+echo "2.检查本地Python版本 $(python -V &2>1)"
 python -V 2>&1 | grep "$version"
 if [ $? -eq 0 ]; then
     echo "当前python版本已经和设定的版本一致，不需要更新了"
     exit 0
 fi
+
+# 安装需要的工具或库文件
+echo "3.安装需要的工具或库文件"
+yum groupinstall -y "Development tools"
+yum install -y openssl-devel zlib-devel ncurses-devel bzip2-devel readline-devel
+yum install -y libtool-ltdl-devel sqlite-devel tk-devel tcl-devel xz-libs
+sleep 5
 
 echo "下载并安装Python"
 wget --no-check-certificate $python_url
